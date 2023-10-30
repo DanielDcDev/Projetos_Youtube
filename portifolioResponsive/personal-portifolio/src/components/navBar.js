d51da12d9cb1f7c90c65867dec5d97d4d1583c7c
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-10-30 12:57:34 +0000

    update app.js

--- portifolioResponsive/personal-portifolio/src/components/navBar.js
+++ portifolioResponsive/personal-portifolio/src/components/navBar.js
@@ -1,7 +1,25 @@
 import {navBar, Container, Navbar} from "react-bootstrap"
+import { useState, useEffect } from "react"
 export const navBar = () =>{
+    const [activeLink, setActiveLink] = useState('home');
+    const [scolled, setScrolled] =useState(false);
+
+        useEffect(() =>{
+            const onScroll =() =>{
+                if(window.scrollY > 50){
+                    setScrolled(true);
+                }else{
+                    setScrolled(false);
+                }
+            } 
+            window.addEventListener("scroll", onScroll);
+
+            return() => window.removeEventListener("scroll", onScroll);
+        }, [])
+
+
 return(
-        <navBar expand="lg">
+        <navBar expand="lg" className∑>
             <Container>
             <navBar.brand href="#home">
                 <img src={''} alt="Logo"/>
